4940d791937077ec7aeb05bb4499d43a7574275e
Zachary Rener <me@zacharyrener.com>
2024-10-16 23:48:45 -0500

    add staging site notice to admin

--- wp-content/themes/zacharyrener/functions.php
+++ wp-content/themes/zacharyrener/functions.php
@@ -269,3 +269,61 @@ function custom_acf_wysiwyg_p_tags($value, $post_id, $field) {
 
 // Hook into ACF's load_value filter for WYSIWYG fields
 add_filter('acf/load_value/type=wysiwyg', 'custom_acf_wysiwyg_p_tags', 10, 3);
+
+
+// [for wpengine] if the environment is staging, add a notice to the admin bar
+// to locally test, add define('WPENGINE_STAGING', true); to wp-config.php
+function add_staging_notice_to_admin_bar() {
+	if (defined('WPENGINE_STAGING') && WPENGINE_STAGING) {
+		global $wp_admin_bar;
+		$wp_admin_bar->add_menu(array(
+			'id' => 'staging-notice',
+			'title' => 'Staging Site',
+			'href' => '',
+			'meta' => array(
+				'class' => 'staging-notice',
+			),
+		));
+	}
+}
+add_action('admin_bar_menu', 'add_staging_notice_to_admin_bar', 999);
+
+function enqueue_staging_admin_styles() {
+    if (defined('WPENGINE_STAGING') && WPENGINE_STAGING) {
+        echo '<style>
+            #wpadminbar ul#wp-admin-bar-root-default>li#wp-admin-bar-staging-notice {
+				position: absolute;
+				left: 0;
+				height: var(--alertBarHeight);
+				top: calc(-1 * var(--alertBarHeight));
+				width: 100%;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				background: #d91f41;
+				color: white;
+				text-align: center;
+				font-size: 16px;
+			}
+
+			div#wpadminbar {
+				margin-top: var(--alertBarHeight);
+			}
+
+			div#wpcontent {
+				margin-top: var(--alertBarHeight);
+			}
+
+			div#wpwrap {
+				--alertBarHeight: 34px;
+			}
+
+			#wpadminbar ul#wp-admin-bar-root-default>li#wp-admin-bar-staging-notice:hover > div {
+				background: transparent;
+				color: white;
+			}
+
+        </style>';
+    }
+}
+add_action('admin_enqueue_scripts', 'enqueue_staging_admin_styles');
